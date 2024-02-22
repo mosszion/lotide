4ca40ec6a50c35256e1ddda7dfbce24e6193f445
Mussie <musdzion@gmail.com>
2024-02-22 17:56:24 -0500

    "better assertEqual"

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function (actual, expected) {
+  let msgp = "✅✅✅✅ Assertion Passed: " + actual + " === " + expected ;
+  let msgf = "❌❌❌❌ Assertion Failed: " + actual + " !== " + expected ;
+  if(actual === expected) {
+    console.log(msgp);
+  }
+  else {
+    console.log(msgf);
+  }
+
+  
+};
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
