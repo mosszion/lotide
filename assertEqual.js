7884a55816fba27356c02e382056fbfc9ec4c9e3
mosszion <musdzion@gmail.com>
2024-02-23 02:09:29 +0000

    fancier assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,6 +1,6 @@
 const assertEqual = function(actual, expected) {
-  let msgp = "✅✅✅✅ Assertion Passed: " + actual + " === " + expected;
-  let msgf = "❌❌❌❌ Assertion Failed: " + actual + " !== " + expected;
+  let msgp = `✅✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  let msgf = `❌❌❌❌ Assertion Failed: ${actual} !== ${expected}`;
   if (actual === expected) {
     console.log(msgp);
  
