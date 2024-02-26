e3631d8c94fe080a15dab2630be492bdeac37ee5
mosszion <musdzion@gmail.com>
2024-02-26 17:16:03 +0000

    checking equal arrays

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  let msgp = `✅✅✅✅ Assertion Passed:  ${actual} === ${expected}`;
+  let msgf = `❌❌❌❌ Assertion Failed:  ${actual} !== ${expected}`;
+  if (actual === expected) {
+    console.log(msgp);
+ 
+  } else {
+    console.log(msgf);
+  }
+
+  
+};
+
+const eqArrays = function(firstArr, secondArr) {
+  for(let i = 0; i < firstArr.length; i++){
+    if(firstArr[i] === secondArr[i]){
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+
+
+
+console.log(eqArrays([1,2,3],[1,2,3]));
+
+assertEqual (eqArrays([1,2,3],[1,2,3]),true);
