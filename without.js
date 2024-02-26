c2bfbe08cb22fc089993b87041b7abd22c0baf02
mosszion <musdzion@gmail.com>
2024-02-26 21:10:37 +0000

    filtering unwanted elements in arrays

---
+++ without.js
@@ -0,0 +1,54 @@
+const eqArrays = function(firstArr, secondArr) {
+  if (firstArr.length === secondArr.length) {
+    let isEqual = true;
+    for (let i = 0; i < firstArr.length; i++) {
+    
+      if (firstArr[i] !== secondArr[i]) {
+        isEqual = false;
+      }
+    }
+    return isEqual;
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  let result = eqArrays(arr1, arr2);
+  if (result) {
+    console.log(`✅✅✅ Test Passed: Arrays are equal.`);
+  } else {
+    console.log(`❌❌❌ Test Failed: Arrays are not equal.`);
+  }
+};
+
+const without = function(array1, array2) {
+  let result = [];
+  
+  for (let i = 0; i < array1.length; i++) {
+    let isFound = false;
+    for (let j = 0; j < array2.length; j++) {
+      if (array1[i] === array2[j]) {
+        isFound = true;
+     
+     
+      }
+
+
+    
+    }
+    if (!isFound) {
+      result.push(array1[i]);
+    }
+   
+  }
+
+  console.log(result);
+};
+
+
+without([1,2,3,4], [2,4]);
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]);
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
