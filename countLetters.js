c8a7afa8dea4edc1fb40c83a16fd6dbe020f828b
mosszion <musdzion@gmail.com>
2024-02-28 16:01:54 +0000

    letter counting

---
+++ countLetters.js
@@ -0,0 +1,43 @@
+
+const assertEqual = function(actual, expected) {
+  
+  if (actual === expected) {
+    console.log(`✅✅✅✅ Assertion Passed:  ${actual} === ${expected}`);
+
+  } else {
+    console.log(`❌❌❌❌ Assertion Failed:  ${actual} !== ${expected}`);
+  }
+
+
+};
+const countLetter = function(str) {
+  const allLetters = [];
+  for (let letter of str) {
+    allLetters.push(letter);
+  }
+  const result = { };
+ 
+  for (let item of allLetters) {
+    if (item !== ' ') {
+      result[item] = (result[item] || 0) + 1;
+
+    }
+    
+  }
+  return result;
+};
+
+console.log(countLetter("this is mine"));
+console.log(countLetter("lighthouse in the house"));
+
+const result1 = countLetter("lighthouse in the house");
+assertEqual(result1["l"], 1);
+assertEqual(result1["i"], 2);
+assertEqual(result1["g"], 1);
+assertEqual(result1["h"], 4);
+assertEqual(result1["t"], 2);
+assertEqual(result1["o"], 2);
+assertEqual(result1["u"], 2);
+assertEqual(result1["s"], 2);
+assertEqual(result1["e"], 3);
+assertEqual(result1["n"], 1);
