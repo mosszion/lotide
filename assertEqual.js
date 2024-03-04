8ee9a15ebb6e44f06cde8e448b2846b392553dd3
Mussie <musdzion@gmail.com>
2024-03-04 15:59:54 -0500

    assertEqual refactored

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,10 @@
-const assertEqual = function(actual, expected) {
-  let msgp = `✅✅✅✅ Assertion Passed: ${actual} === ${expected}`;
-  let msgf = `❌❌❌❌ Assertion Failed: ${actual} !== ${expected}`;
-  if (actual === expected) {
-    console.log(msgp);
+const assertEqual = function(actual, expected) {  // assertEqual takes two arrays actual and expected
+  
+  if (actual === expected) {        //checks if they are equal
+    console.log(`✅✅✅✅ Assertion Passed: ${actual} === ${expected}`); //consoles pass message
  
   } else {
-    console.log(msgf);
+    console.log(`❌❌❌❌ Assertion Failed: ${actual} !== ${expected}`); //consoles fail message
   }
 
   
