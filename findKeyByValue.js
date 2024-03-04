c960f5586ca3ab2fedf05fe725ce7a173b174014
Mussie <musdzion@gmail.com>
2024-03-04 16:12:31 -0500

    Refactored findKeyByValue

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,16 +1,14 @@
 //asertEqual function block
-const assertEqual = function(actual, expected) {
+const assertEqual = function(actual, expected) {  // assertEqual takes two literals, actual and expected
+  
+  if (actual === expected) {        //checks if they are equal
+    console.log(`✅✅✅✅ Assertion Passed: ${actual} === ${expected}`); //consoles pass message
  
-  if (actual === expected) {
-
-    console.log(`✅✅✅✅ Assertion Passed:  ${actual} === ${expected}`);
-
   } else {
-
-    console.log(`❌❌❌❌ Assertion Failed:  ${actual} !== ${expected}`);
+    console.log(`❌❌❌❌ Assertion Failed: ${actual} !== ${expected}`); //consoles fail message
   }
 
-
+  
 };
 
 
@@ -19,7 +17,7 @@ const assertEqual = function(actual, expected) {
 // Then if the value is found returns its key
 
 const findKeyByValue = function(obj, value) {  //find key takes  two arguments : one object and one value
-  let  keyValue ;
+ 
   const keysOfObj = Object.keys(obj);  //puts all keys into keysOfObj array
   
   for (let key of keysOfObj) { //iterate through the elements in keysOfObj
@@ -31,7 +29,8 @@ const findKeyByValue = function(obj, value) {  //find key takes  two arguments :
       return key; //returns the key if found true 
     }
   }
-  return keyValue; // returns the key
+  
+
   
 };
 
