c7bcaea2df83ccd777bcbcdab67b63eb40715580
Mussie <musdzion@gmail.com>
2024-03-04 16:11:47 -0500

    assertEqual better commented

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,4 @@
-const assertEqual = function(actual, expected) {  // assertEqual takes two arrays actual and expected
+const assertEqual = function(actual, expected) {  // assertEqual takes two literals, actual and expected
   
   if (actual === expected) {        //checks if they are equal
     console.log(`✅✅✅✅ Assertion Passed: ${actual} === ${expected}`); //consoles pass message
