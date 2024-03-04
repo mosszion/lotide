2f8c073b114aec515a0602e4e3ead4ee7d379043
Mussie <musdzion@gmail.com>
2024-03-04 16:00:32 -0500

    findKeyByValue refactored

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,4 +1,4 @@
-
+//asertEqual function block
 const assertEqual = function(actual, expected) {
  
   if (actual === expected) {
@@ -13,29 +13,35 @@ const assertEqual = function(actual, expected) {
 
 };
 
-const findKeyByValue = function(obj, value) {
-  let  keyValue = undefined;
-  const keysOfObj = Object.keys(obj);
+
+//findKeyByValue is a function which takes two arguments one Object and a value
+// checks if the value is one of the values in the object 
+// Then if the value is found returns its key
+
+const findKeyByValue = function(obj, value) {  //find key takes  two arguments : one object and one value
+  let  keyValue ;
+  const keysOfObj = Object.keys(obj);  //puts all keys into keysOfObj array
   
-  for (let key of keysOfObj) {
+  for (let key of keysOfObj) { //iterate through the elements in keysOfObj
     
-    let valOfObj = obj[key];
+    let valOfObj = obj[key];  //assigns obj[key] to valOfObj 
     
    
-    if (valOfObj === value) {
-      keyValue = key;
+    if (valOfObj === value) { // checks the value of the key with the value given in the argument
+      return key; //returns the key if found true 
     }
   }
-  return keyValue;
+  return keyValue; // returns the key
   
 };
 
-findKeyByValue({a:1,b:3,c:4},3);
+
+
 const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
 };
 findKeyByValue(bestTVShowsByGenre, "The Wire");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"); // Pass
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined); //Pass
