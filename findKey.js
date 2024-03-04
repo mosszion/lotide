0348b3687100b11fb60ab29111ec64526672a63d
Mussie <musdzion@gmail.com>
2024-03-04 16:12:15 -0500

    refactored findKey

--- findKey.js
+++ findKey.js
@@ -1,6 +1,6 @@
 //assertEqual function block
 
-const assertEqual = function(actual, expected) {  // assertEqual takes two arrays actual and expected
+const assertEqual = function(actual, expected) {  // assertEqual takes two literals, actual and expected
   
   if (actual === expected) {        //checks if they are equal
     console.log(`✅✅✅✅ Assertion Passed: ${actual} === ${expected}`); //consoles pass message
@@ -22,7 +22,7 @@ const assertEqual = function(actual, expected) {  // assertEqual takes two array
 
 //lets intialize the function
 const findKey = function (object, callback) {
-  let keyFound;
+ 
   const keysOfObj = Object.keys(object);  //puts all keys into keysOfObj array
   
   for (let key of keysOfObj) { //iterate through the elements in keysOfObj
@@ -34,7 +34,7 @@ const findKey = function (object, callback) {
       return key; //returns the key if found true 
     }
   }
-  return keyFound; // returns the key
+
   
 
 
@@ -45,13 +45,36 @@ const findKey = function (object, callback) {
 
 
 
-
-
-console.log(findKey({
+assertEqual(findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2)) // => "noma"
+}, x => x.stars === 2), "noma") //Pass
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 5), undefined) //Pass
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3), "Akaleri") // Pass
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3), "elBulli") // => Fail
