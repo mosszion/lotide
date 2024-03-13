19e1a30813575363cbd30b7b480758ebc938a34f
Mussie <musdzion@gmail.com>
2024-03-13 05:12:42 -0400

    refactored tail

--- tail.js
+++ tail.js
@@ -1,26 +1,15 @@
-const assertEqual = function(actual, expected) {
- 
-  if (actual === expected) {
-    console.log(`✅✅✅✅ Assertion Passed: ${actual} === ${expected}`);
- 
-  } else {
-    console.log(`❌❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
 
-  
-};
-
-const tail = function(word) {
-  if (word.length <= 1) {
-    return [];
+const tail = function(word) { //takes input array
+  if (word.length <= 1) {     //checks array elements
+    return [];                //returns empty array 
 
   } else {
     const tailed = [];
   
-    for (let i = 1; i < word.length; i++) {
-      tailed.push(word[i]);
+    for (let i = 1; i < word.length; i++) {  //starts looping from i[1]
+      tailed.push(word[i]);                  //pushes every word
     }
-    return tailed;
+    return tailed;                           //returns tailed array
 
   }
   
@@ -28,13 +17,4 @@ const tail = function(word) {
 };
 
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-const result2 = tail(["Hello"]);
-console.log(result2);
-assertEqual(result.length, 2);
-assertEqual(result[0],"Lighthouse");
-assertEqual(result[1],"Labs");
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-const newWord = tail(words);
-assertEqual(words.length, 3);
-assertEqual(newWord.length, 2);
+module.exports = tail; 
