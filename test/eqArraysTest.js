7ab0821d41021f504334fc2d88db3fd7df907927
Mussie <musdzion@gmail.com>
2024-03-13 05:40:24 -0400

    modified testing

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -2,6 +2,10 @@ const eqArrays = require ('../eqArrays');         //import eqArray fn
 const assertEqual = require ('../assertEqual');   //import assertEqual fn
 
 
+// Test cases
 console.log(eqArrays([1,2,3],[1,2,3]));          //True
-
 assertEqual (eqArrays([1,2,3],[1,2,3]),true);    //Pass
+
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);  //Pass
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);  //Pass
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);  //Fail
