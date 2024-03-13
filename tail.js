ac020e7604fb41450c52f4a2c91e31d3efaaa457
Mussie <musdzion@gmail.com>
2024-03-13 05:23:56 -0400

    addtional refactoring

--- tail.js
+++ tail.js
@@ -1,13 +1,8 @@
 
 const tail = function(word) { //takes input array
-  if (word.length <= 1) {     //checks array elements
-    return [];                //returns empty array 
-
-  } else {
-
+  
     return word.slice(1)         //return slice from 1            
 
-  }
   
  
 };
