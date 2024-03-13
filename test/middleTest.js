1fa3325c03e463991bde6b5978eb8166c7972e39
Mussie <musdzion@gmail.com>
2024-03-13 09:41:25 -0400

    additional corrections

--- test/middleTest.js
+++ test/middleTest.js
@@ -8,11 +8,17 @@ const middle = require('../middle');          //Imports middle fn
 //Test cases:
 
 describe("#middle", () => {
-  it("returns true if middle of [1,2,3] is [2]", () => {
+  it("returns [2] for middle of [1,2,3] ", () => {
     assert.deepEqual(middle([1,2,3]),[2]); //isTrue used for equal arrays
   });
-  it("returns true if middle of [1,2,3,4] is [2,3]", () => {
+  it("returns [2,3] for middle of [1,2,3,4]", () => {
     assert.deepEqual(middle([1,2,3,4]),[2,3]);
+  })
+  it("returns [] for middle of [1]", () => {
+    assert.deepEqual(middle([1]),[]);
+  })
+  it("returns [5,7] for middle of [3,5,7,4]", () => {
+    assert.deepEqual(middle([3,5,7,4]),[5,7]);
   })
 });
 
--- test/tailTest.js
+++ test/tailTest.js
@@ -6,11 +6,17 @@ const assert = require ('chai').assert; //import assert from chai fn
 //Test cases:
 
 describe("#tail", () => {
-  it("returns [2,3] for [1,2,3]", () => {
+  it("returns [2,3] for tail [1,2,3]", () => {
     assert.deepEqual(tail([1,2,3]), [2,3]); //deepEqual used for equal arrays
   });
-  it("returns [] for ['5']", () => {
+  it("returns [] for tail ['5']", () => {
     assert.deepEqual(tail(['5']), []);
+  })
+  it("returns [5,7,4] for tail of [3,5,7,4]", () => {
+    assert.deepEqual(tail([3,5,7,4]),[5,7,4]);
+  })
+  it("returns [7,6,5,4,3,2,1] for tail of [8,7,6,5,4,3,2,1]", () => {
+    assert.deepEqual(tail([8,7,6,5,4,3,2,1]),[7,6,5,4,3,2,1]);
   })
 });
 
