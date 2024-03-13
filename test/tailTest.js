998fc8ab15874bc1d37f7a6066d313c0ed3b31c7
Mussie <musdzion@gmail.com>
2024-03-13 09:31:30 -0400

    tail correction

--- test/tailTest.js
+++ test/tailTest.js
@@ -9,7 +9,7 @@ describe("#tail", () => {
   it("returns [2,3] for [1,2,3]", () => {
     assert.deepEqual(tail([1,2,3]), [2,3]); //deepEqual used for equal arrays
   });
-  it("returns ['5'] for []", () => {
+  it("returns [] for ['5']", () => {
     assert.deepEqual(tail(['5']), []);
   })
 });
